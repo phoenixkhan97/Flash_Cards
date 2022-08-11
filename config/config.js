c99b84cdb6951a69a447a9ea321460647d226f72
Yun <yuniya.pa98@gmail.com>
2022-08-11 14:07:43 -0400

    creating models

--- config/config.js
+++
@@ -1,8 +0,0 @@
-const config = require('./config')
-
-module.exports = {
-    database: "flash_cards",
-    username: "black",
-    password: "1234",
-    dialect: "postgres",
-    host: "127.0.0.1"}
