4a7cdb7e117f7ce8427eb607c38139d3eb01244f
jay <jayleungg@gmail.com>
2022-08-11 15:05:30 -0400

    routes working

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 import Nav from "./components/Nav";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AddCard from "./pages/AddCard";
+import Card from "./pages/Card";
+import EditCard from "./pages/EditCard";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Quiz from "./pages/Quiz";
+import Register from "./pages/Register";
+
+const linkStyle = {
+  margin: "1rem",
+  textDecoration: "none",
+  color: "white",
+};
 
 function App() {
   return (
@@ -7,6 +22,18 @@ function App() {
       <nav>
         <Nav />
       </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/AddCard" element={<AddCard />} />
+          <Route path="/Card" element={<Card />} />
+          <Route path="/Card/:id" element={<EditCard />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/Card/Quiz" element={<Quiz />} />
+          <Route path="/Register" element={<Register />} />
+        </Routes>
+      </main>
     </div>
   );
 }
