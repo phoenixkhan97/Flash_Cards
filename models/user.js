d64a975ba07b7eead7303a2ebf404c61b796e0a1
Yun <yuniya.pa98@gmail.com>
2022-08-11 14:32:11 -0400

    models and relationships created

--- models/user.js
+++ models/user.js
@@ -8,7 +8,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.hasMany(models.Card, { foreignKey: 'cretorId' })
+      User.belongsToMany(models.Card, {
+        through: models.Library,
+        as: 'library',
+        foreignKey: 'userId'
+      })
     }
   }
   User.init(
