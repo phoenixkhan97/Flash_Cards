894df663118b05d7b772c7df018d013bfdce1f76
Yun <yuniya.pa98@gmail.com>
2022-08-11 18:07:45 -0400

    Working on express

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -2,5 +2,7 @@ const Router = require('express').Router()
 const controller = require('../controller/authController')
 const middleware = require('../middleware')
 
-router.post('/login', controller.Login)
-router.post('/register', controller.Register)
+Router.post('/login', controller.Login)
+Router.post('/register', controller.Register)
+
+module.exports = Router
