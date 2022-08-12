77392b33489f4941baed9a0079901a2356a240d7
jay <jayleungg@gmail.com>
2022-08-12 15:17:42 -0400

    update

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,10 +3,18 @@ function Nav() {
   return (
     <nav className="nav-bar">
       <h1 className="nav-title">Quizards!</h1>
-      <div className = 'nav-link' >
-        <NavLink className = 'nav-link1' to="/">Home |</NavLink>
-        <NavLink className = 'nav-link1' to="/Register"> Sign up |</NavLink>
-        <NavLink className = 'nav-link1' to="/Login"> Login</NavLink>
+      <div className="nav-link">
+        <NavLink className="nav-link1" to="/">
+          Home |
+        </NavLink>
+        <NavLink className="nav-link1" to="/Register">
+          {" "}
+          Sign up |
+        </NavLink>
+        <NavLink className="nav-link1" to="/Login">
+          {" "}
+          Login
+        </NavLink>
       </div>
     </nav>
   );
