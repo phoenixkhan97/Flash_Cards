2646cd3fc544472345efbecdc78abf4ad243efde
Yun <yuniya.pa98@gmail.com>
2022-08-16 11:01:20 -0400

    pervent using routes without authentication

--- routes/CardRouter.js
+++ routes/CardRouter.js
@@ -1,13 +1,49 @@
 const Router = require('express').Router()
 const controller = require('../controller/cardController')
+const middleware = require('../middleware')
 
-Router.get('/:card_id', controller.getCardById)
-Router.get('/card/:user_id', controller.getAllTypes)
-Router.get('/cards/:type', controller.getAllByType)
-Router.get('/find/:user_id/:type', controller.getAllByTypeAndUserId)
-Router.post('/:user_id', controller.createCard)
+Router.get(
+  '/:card_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.getCardById
+)
+Router.get(
+  '/card/:user_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.getAllTypes
+)
+Router.get(
+  '/cards/:type',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.getAllByType
+)
+Router.get(
+  '/find/:user_id/:type',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.getAllByTypeAndUserId
+)
+Router.post(
+  '/:user_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.createCard
+)
 
-Router.put('/:card_id', controller.updateCard)
+Router.put(
+  '/:card_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.updateCard
+)
 
-Router.delete('/:card_id', controller.deleteCard)
+Router.delete(
+  '/:card_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.deleteCard
+)
 module.exports = Router
